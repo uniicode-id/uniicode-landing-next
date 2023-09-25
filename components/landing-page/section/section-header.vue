2f96b701ed7d5d059396cd96cf6871045bf38c67
Dimar Hanung P <dimarhanung@gmail.com>
2023-09-26 03:15:41 +0700

    feat: init header

--- components/landing-page/section/section-header.vue
+++ components/landing-page/section/section-header.vue
@@ -1,4 +1,21 @@
 <template>
-   <header class="p-4">Section Header</header>
+  <header class="p-4 min-h-screen grid place-items-center">
+    <div>
+      <h1 class="text-6xl text-center font-bold">
+        Where Programming<br />
+        Meets Fun!
+      </h1>
+      <h2 class="text-2xl text-center text-[#777] mt-8">
+        Foster Creativity, Build Connections, and Elevate Design in Our
+        Community.
+      </h2>
+
+      <button
+        class="mt-12 bg-[#CCFF00] text-uc-7 px-8 py-4 rounded-lg font-bold block mx-auto"
+      >
+        Learn More
+      </button>
+    </div>
+  </header>
 </template>
 <script setup lang="ts"></script>
--- pages/landing-page/index.vue
+++ pages/landing-page/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-uc-7 text-slate-100">
-    <section-navbar />
+  <div class="landing-container bg-uc-7 text-slate-100">
+    <section-navbar class="fixed top-0 w-full" />
     <section-header />
     <section-about />
     <section-member />
@@ -10,3 +10,9 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped>
+.landing-container {
+  font-family: Helvetica, Arial, sans-serif;
+}
+</style>
