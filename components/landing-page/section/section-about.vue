c7beb8a1c84d63a1ce59dc2a478297901f6583d5
Dimar Hanung P <dimarhanung@gmail.com>
2023-09-26 03:27:28 +0700

    feat: init about section

--- components/landing-page/section/section-about.vue
+++ components/landing-page/section/section-about.vue
@@ -1,8 +1,61 @@
 <template>
-  <main class="text-red-800 p-2 border border-black">
+  <main class="p-2 border border-black">
     <decor-about-bg />
 
-    <p>Section About ss</p>
+    <div class="text-center">
+      <h2 class="text-5xl font-bold">WHAT ABOUT OF UNIICODE?</h2>
+      <p class="text-x; mt-8 max-w-4xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur. Vel faucibus aliquam posuere
+        lectus odio laoreet. Pharetra sodales gravida luctus neque nunc
+        placerat. Vel felis ut nulla eget ultrices cras duis mauris interdum.
+        Tortor sapien sagittis pulvinar et mattis sed.
+      </p>
+    </div>
+
+    <div class="container mx-auto mt-24">
+      <div class="flex gap-8">
+        <div>
+          <icons-uc-vision />
+        </div>
+        <div>
+          <h2 class="text-5xl font-bold uppercase">Vision</h2>
+          <p class="mt-8 text-xl">
+            Lorem ipsum dolor sit amet consectetur. Duis ac nibh nisl nibh in
+            dolor et. Velit pretium ultricies etiam suspendisse ac arcu sit.
+            Proin justo id montes vestibulum. Diam nisi eu elementum venenatis
+            id quam. Gravida sed tincidunt pellentesque egestas nec tincidunt.
+          </p>
+        </div>
+      </div>
+      <div class="flex gap-8 mt-14">
+        <div>
+          <icons-uc-mission />
+        </div>
+        <div>
+          <h2 class="text-5xl font-bold uppercase">Mission</h2>
+          <p class="mt-8 text-xl">
+            Lorem ipsum dolor sit amet consectetur. Duis ac nibh nisl nibh in
+            dolor et. Velit pretium ultricies etiam suspendisse ac arcu sit.
+            Proin justo id montes vestibulum. Diam nisi eu elementum venenatis
+            id quam. Gravida sed tincidunt pellentesque egestas nec tincidunt.
+          </p>
+        </div>
+      </div>
+      <div class="flex gap-8 mt-14">
+        <div>
+          <icons-uc-purpose />
+        </div>
+        <div>
+          <h2 class="text-5xl font-bold uppercase">Purpose</h2>
+          <p class="mt-8 text-xl">
+            Lorem ipsum dolor sit amet consectetur. Duis ac nibh nisl nibh in
+            dolor et. Velit pretium ultricies etiam suspendisse ac arcu sit.
+            Proin justo id montes vestibulum. Diam nisi eu elementum venenatis
+            id quam. Gravida sed tincidunt pellentesque egestas nec tincidunt.
+          </p>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
